aa6e2835ba96066c4e9e9a815318307a1d4d30c0
offsujin <offsujin@gmail.com>
2022-02-28 14:31:53 +0900

    Init project

---
+++ src/main/java/com/covid/resv/CovidResvApplication.java
@@ -0,0 +1,13 @@
+package com.covid.resv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidResvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidResvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid/resv/CovidResvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.resv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidResvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

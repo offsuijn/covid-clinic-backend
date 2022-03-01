cbd4a2113d3fb31fedb2970d3535dee29f00092b
offsujin <offsujin@gmail.com>
2022-03-01 22:47:22 +0900

    add security dto

---
+++ src/main/java/com/covid/resv/controller/token/TokenRequestDto.java
@@ -0,0 +1,10 @@
+package com.covid.resv.controller.token;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenRequestDto {
+    private String accessToken;
+}
